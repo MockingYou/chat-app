3863bc3bbeef1c6140dbde0451aac9bd5b3e42d2
Vlad <vlad.mocanu13@gmail.com>
2023-02-25 14:34:19 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const { Server } = require("socket.io");
 const cors = require("cors")
 const Filter = require('bad-words')
 const { generateMessage } = require('./utils/messages')
@@ -10,11 +11,12 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users'
 
 const app = express();
 const server = http.createServer(app)
-const io = socketio(server, {
-    cors: {
-        origins: '*:*'
-    }
-})
+// const io = socketio(server, {
+//     cors: {
+//         origins: '*:*'
+//     }
+// })
+const io = new Server(server);
 
 const port = process.env.PORT || 5000;
 const publicDirectoryPath = path.join(__dirname, '../public')
